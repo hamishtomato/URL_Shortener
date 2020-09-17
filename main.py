2439a1acc1d0b2b9c1101dd69e3b3c4dd5f82c97
hamishtomato <hamishnewid@gmail.com>
2020-09-17 17:16:52 +0800

    [feature] Return the full URL

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import re
 import hashlib
 import base62
+from urllib.parse import urlparse
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
+from starlette.requests import Request
 
 import crud, models, schemas
 from database import SessionLocal, engine
@@ -24,16 +26,19 @@ def get_db():
         db.close()
 
 @app.post("/urls/register")
-async def register_url(body: schemas.UrlCreate, db: Session = Depends(get_db)):
+async def register_url(request: Request, body: schemas.UrlCreate, db: Session = Depends(get_db)):
     if not re.match(r'^https?:/{2}\w.+$', body.url):
         raise HTTPException(status_code=400, detail="URL is invalid")
 
     row = crud.get_short_url(db, origin_url=body.url)
     if row:
         return row.short_url
-    short_url = short(body.url)
-    create_url = crud.create_url(short_url=short_url, origin_url=body.url, db=db)
-    return create_url.short_url
+
+    short_url_path = short(body.url)
+    url_parse = urlparse(str(request.url))
+    crud.create_url(short_url=short_url_path, origin_url=body.url, db=db)
+    short_url = '{}://{}/{}'.format(url_parse.scheme , url_parse.netloc , short_url_path)
+    return short_url
 
 @app.get("/{short_url}")
 def read_users(short_url, db: Session = Depends(get_db)):
