360da04a283ec5dba81feb94b984465bd879e260
hamish_wu <hamish.wu@metropia.com>
2020-09-19 18:53:04 +0800

    [bin] Change database

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgres://wayrdthwudihxs:928c480a6b72ffae76da47d13bcab7983c341a6354de1f7af18113353e8e28dc@ec2-52-200-134-180.compute-1.amazonaws.com:5432/d4rdojto7v9b8a"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
